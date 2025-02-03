ced2e1213cd3c0690d9b5bae8e09c9dad8f299e6
1Andria <andriamelua22@gmail.com>
2025-02-03 17:40:54 +0400

    main

--- src/__Organism/Container/Container.jsx
+++ src/__Organism/Container/Container.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Container.css";
 import CardSide from "../../__Molecule/CardSide/CardSide";
 import Informational from "../../__Molecule/Informational/Informational";
 
 function Container() {
+  useEffect(() => {
+    document.title = "Card Form";
+  }, []);
   const [cardData, setCardData] = useState({
     name: "JANE APPLESEED",
     number: "0000 0000 0000 0000",
